d199dace8d08d1af27fd678ef00e0c94a9bdc3e1
DanaiLathong <bankkok159z@gmail.com>
2023-10-27 10:40:30 +0700

    Code2 : Dictionary

---
+++ test02.py
@@ -0,0 +1,48 @@
+# Dictionary คือ key : value
+my_dict1 = { "name":"mod" , "age":30 , 555:999 , "flag":True , "wow":[ 10 , 20 , 30 ]}
+my_dict2 = { "data1":[ 10 , 30 , 40 ] , 
+            "data2":( 2 , 5 , 6 ) , 
+            "data3":( 45 , 3 , 4 ) , 
+            "data4":{ "x":111 , 
+                     "y":222 }
+            }
+#การเข้าถึงข้อมูลใดข้อมูลหนึ่ง
+print (my_dict1["name"])
+print (my_dict1["age"])
+print (my_dict1[555])
+my_dict1["age"] = 50
+print (my_dict1["age"])
+#อยากแสดงผล 20 ออกมาทำไง
+print (my_dict1['wow'][1])
+#อยากแสดงผล 222 ออกมาทำไง
+print(my_dict2["data4"]['y'])
+print(my_dict2)
+my_dict2['data5'] = 888
+print(my_dict2)
+
+#การเข้าถึงทุกข้อมูล
+for xx in my_dict1 :
+    print(xx)
+
+print('---------------------')
+for xx in my_dict1.keys(): 
+    print(xx)
+
+print('---------------------')
+for xx in my_dict1.values():
+    print(xx)
+
+print('---------------------')
+for xx,yy in my_dict1.items():
+    print(xx, '-' ,yy)
+
+# Dictionary method
+my_dict1.popitem()
+my_dict1.popitem()
+my_dict1.popitem()
+print(my_dict1)
+my_dict2.pop("data2")
+print(my_dict2)
+print('----------------------')
+print(my_dict2['data2'])
+print(my_dict2.gat['data2'])
